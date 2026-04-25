5c471d9cccad1fd42ff7955163370d1f2a58ebd4
Ahmed Gamal <a.ibrahim@noqoodholding.com>
2026-04-25 16:38:05 +0300

    feat(routes): enable authGuard for the main route to enforce access control

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -54,7 +54,7 @@ export const routes: Routes = [
     },
     {
         path: '',
-        // canActivate: [authGuard],
+        canActivate: [authGuard],
         loadComponent: () => import('./layout/layout.component').then(m => m.LayoutComponent),
         children: [
             {
